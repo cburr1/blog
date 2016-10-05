7e66b6f1330b3be0e9bb753868eeccc7d928896d
Cory Burr <cburr1@uncc.edu>
2016-10-05 20:45:45 +0000

    5.9 Finish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,14 @@
 class ArticlesController < ApplicationController
+    
+    
+    def index
+        @articles = Article.all
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
     def new
     end
     
@@ -11,7 +21,9 @@ class ArticlesController < ApplicationController
  
     private
         def article_params
-            params.require(:article).permit(:title, :text)
+        params.require(:article).permit(:title, :text)
         end
+    
+
 end
     
